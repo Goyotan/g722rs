93d7a55279580d3bdadfd7d26a0ae70a62fda1d2
Goyotan <skyline38thaxus@gmail.com>
2023-03-10 01:42:01 +0900

    add g722_decode ffi

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
     , dead_code)]
 
 use std::ffi::c_void;
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
-use std::ptr::null;
 use std::{env, process::exit};
 use std::fs;
 
@@ -18,11 +18,13 @@ fn main() {
         exit(-1);
     }
 
-    let filepath = args.get(0).unwrap();
+    let filepath = args.get(1).unwrap();
     println!("[*] filepath: {}", filepath);
     let pathbuf = PathBuf::from(filepath);
     let abspath = fs::canonicalize(pathbuf).unwrap();
-    let _rawdata = fs::read(abspath).unwrap();
+    let rawdata = fs::read(abspath).unwrap();
+
+    let mut writer = BufWriter::new(fs::File::create("./test.raw").unwrap());
 
     const BUF_SIZE: usize = 10;
     let mut ibuf: [u8; BUF_SIZE] = Default::default();
@@ -35,9 +37,20 @@ fn main() {
             eprintln!("[!] g722_decoder_new failed");
             exit(-1);
         }
-        ibuf.clone_from_slice(&_rawdata[0..10]);
-        g722_decode(g722_dctx, ibuf.as_ptr(), BUF_SIZE as i32, obuf.as_ptr() as *mut i16);
+        
+        for i in (0..rawdata.len()).step_by(10) {
+        
+            // copy every 10 bytes
+            ibuf.copy_from_slice(&rawdata[i..i+10]);
 
-        println!("{}", _rawdata[0]);
+            g722_decode(g722_dctx, ibuf.as_ptr(), BUF_SIZE as i32, obuf.as_ptr() as *mut i16);
+            
+            // i16 to u8
+            let (_, shorts, _) = obuf.align_to::<u8>();
+            
+            writer.write(&shorts[0..20]).unwrap();
+        }
+        writer.flush().unwrap();
     }
+
 }
